40d017c97804e43992324ac2bf55d59cb11001ae
ambrojone123 <muhammedshibil56775@gmail.com>
2026-01-08 16:17:14 +0530

    error fixed2

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import { HashRouter } from "react-router-dom";
 
 
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/POS-Web-Application">
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
